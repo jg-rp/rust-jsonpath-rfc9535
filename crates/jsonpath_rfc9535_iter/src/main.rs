896f4450ccbbbf3baf4fdb4d1075d8f487c8611c
James Prior <jamesgr.prior@gmail.com>
2024-06-11 12:32:04 +0100

    Setup for memory profiling [skip ci]

--- crates/jsonpath_rfc9535_iter/src/main.rs
+++ crates/jsonpath_rfc9535_iter/src/main.rs
@@ -1,24 +1,15 @@
+use std::{fs::File, io::BufReader};
+
 use jsonpath_rfc9535_iter::jsonpath::find;
 use serde_json::Value;
 
 fn main() {
-    let data = r#"
-    [
-        {
-          "a": {
-            "b": "foo"
-          }
-        },
-        {}
-      ]"#;
+    let file = File::open("/tmp/datasets/citylots.json").expect("could not open data file");
+    let reader = BufReader::new(file);
+    let v: Value = serde_json::from_reader(reader).expect("error reading data file");
 
-    // Parse the string of data into serde_json::Value.
-    let v: Value = serde_json::from_str(data).unwrap();
-    let q = "$[?length(@ .a .b) == 3]";
-
-    // println!("Q: {:#?}", Query::standard(q));
-
+    let q = "$.features..properties";
     let rv = find(q, &v).unwrap();
     let values: Vec<&Value> = rv.collect();
-    println!("{:#?}", values);
+    println!("{:?}", values.len());
 }
